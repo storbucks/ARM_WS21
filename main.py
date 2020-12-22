2d22935e8743dbaa213f83d6f0aed4fcb1b82d45
storbucks <76474717+storbucks@users.noreply.github.com>
2020-12-22 16:02:28 +0100

    busy testing

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import statsmodels.stats.anova as anova
+import scipy as sci
+
+from sklearn import metrics
 
 # Loading data, just copy the Training_Dataset.csv file into the working directory of your python project:
 traindata = pd.read_csv("Training_Dataset.csv", sep=";")
 
 # Run some checks if you want to:
-# print(traindata.head())
-# print(traindata.tail())
-# print(traindata.isnull().sum().sort_values(ascending=False))
+print(traindata.head())
+print(traindata.tail())
+print(traindata.isnull().sum().sort_values(ascending=False))
 
 # Display information about the dataset at a glance:
 print(traindata.info())  # Output: 40 cols, 669 rows, dtypes: float, int, object(here: strings)
@@ -15,6 +24,9 @@ catvar = [i for i in list(traindata.columns) if traindata[i].dtype=='O']  # cate
 numvar = [i for i in list(traindata.columns) if traindata[i].dtype in ['float64','int64']]  # numerical variables
 
 # Check for missing values:
-# print(traindata.isnull().sum())
-print("Hi bbs")
-print("Test")
+print(traindata.isnull().sum())
+
+# Test OLS regression made by me
+testreg = smf.ols(formula="sales ~ gross_profit", data=traindata)
+res = testreg.fit()
+print(res.summary2())
