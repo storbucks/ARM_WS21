a51dd962191924357397eeb3e4b7902ea1b9d059
helbrecht-h <76485337+helbrecht-h@users.noreply.github.com>
2020-12-22 17:05:36 +0100

    test

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import statsmodels.stats.anova as anova
 import scipy as sci
-
+# hey
 from sklearn import metrics
 
 # Loading data, just copy the Training_Dataset.csv file into the working directory of your python project:
