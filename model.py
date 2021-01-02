a559f388c6c5add9bc15abb043695b4e68e8ef91
storbucks <76474717+storbucks@users.noreply.github.com>
2021-01-02 10:20:43 +0100

    hier model bauen

---
+++ model.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+import statsmodels.stats.anova as anova
+import scipy as sci
+
+from sklearn import metrics
+
+# Loading data, just copy the Training_Dataset.csv file into the working directory of your python project:
+traindata = pd.read_csv("Training_Dataset.csv", sep=";")
+#%%
