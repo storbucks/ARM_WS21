59e868b81c4d7cb7362af845610a1b7d2d9f304c
cedriklamar <76492891+cedriklamar@users.noreply.github.com>
2021-01-04 09:24:12 +0100

    Describe Data

--- playground.py
+++ playground.py
@@ -20,10 +20,11 @@ print(traindata.tail())
 print(traindata.isnull().sum().sort_values(ascending=False))
 
 # Overview NA's
-pd.DataFrame({'Valid': traindata.notnull().sum(),
+print(pd.DataFrame({'Valid': traindata.notnull().sum(),
               'NAs': traindata.isnull().sum(),
               'NAs of total': traindata.isnull().sum() / traindata.shape[0]}
-            ).sort_values('NAs of total', ascending=True)
+            ).sort_values('NAs of total', ascending=True))
+print(traindata.describe())
 
 # Display information about the dataset at a glance:
 print(traindata.info())  # Output: 40 cols, 669 rows, dtypes: float, int, object(here: strings), added: bool (2)
