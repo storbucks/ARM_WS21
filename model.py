16a2227dddb28f7db3b3a7075e36102082edb625
evasophie <76488583+evasophie@users.noreply.github.com>
2021-01-05 10:58:18 +0100

    tests in eigenes file verschoben und model weiter angepasst

--- model.py
+++ model.py
@@ -12,10 +12,12 @@ testdata = pd.read_csv("Test_Dataset.csv", sep=";")
 # functions to analyse data #
 #############################
 
+
 #  change from year of foundation to years since foundation
 def year_inc_transformation(data):
     for i in range(0, len(data.year_inc)):
         data.year_inc[i] = 2021 - data.year_inc[i]
+
 
 #  calculate and save different ratios or indicators
 def create_indicator_frame(data):
@@ -28,15 +30,15 @@ def create_indicator_frame(data):
     equity_ratio = data.total_equity.copy() / data.total_assets.copy()
     ebit_margin = data.earn_from_op.copy() / data.sales.copy()
 
-   # age_level = []
-    #for i in range(0, len(data.year_inc)):
-     #   age_level.append(data.year_inc[i].copy()/oldest_company)
-
+    #  age_level = []
+    #  for i in range(0, len(data.year_inc)):
+    #      age_level.append(data.year_inc[i].copy()/oldest_company)
     frame = {'id': data.id, 'interest_coverage': interest_coverage, 'roa': roa, 'debt_ratio': debt_ratio,
             'debt_to_equity_ratio': debt_to_equity_ratio, 'equity_ratio': equity_ratio,
             'ebit_margin': ebit_margin, 'cf_operating': data.cf_operating, 'current_ratio': current_ratio}
     indicators = pd.DataFrame(frame)
     return indicators
+
 
 #  calculate the values to classify companies in default and non-default
 def calculate_pds(indicators):
@@ -45,6 +47,7 @@ def calculate_pds(indicators):
     estimations = pd.DataFrame(frame)
     estimations['estimated_pd'] = ""
 
+    # classification depends on one liquidity ratio (current assets/current liabs), one leverage ratio (debt ratio) and one profitability ratio (roa)
     for i in range(0, len(indicators['id'])):
         x = -3.4163441714312563 + 0.9435381378963345 * indicators['debt_ratio'][i] - 1.0848159927024499 * indicators['current_ratio'][i] - (0.00010046357934681097/10000) * indicators['roa'][i]
         pi = (np.exp(x)/(1 + np.exp(x)))
@@ -55,12 +58,25 @@ def calculate_pds(indicators):
             nan_index.append(i)  # löschen sobald Funktionen für nan's drin sind
     return estimations
 
+
+def create_default_booleans(estimations):
+    estimations['default_boolean'] = ""
+    default_threshold = 0.05
+    for i in range(0, len(estimations)):
+        if estimations['estimated_pd'][i] >= default_threshold:
+            estimations['default_boolean'] = True
+        else:
+            estimations['default_boolean'] = False
+    return estimations
+
+
 # function that runs all functions for a given dataset
 def pd_estimations(data):
     year_inc_transformation(data)
     indicators = create_indicator_frame(data)
-    estimated_pds = calculate_pds(indicators)
-    return estimated_pds
+    estimations = calculate_pds(indicators)
+    default_booleans = create_default_booleans(estimations)
+    return default_booleans
 
 
 print(pd_estimations(traindata_m))
